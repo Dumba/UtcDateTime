e22329fe0672a1bc4af279c8e9835555dd6580ab
Samuel Lachman <samlachman@centrum.cz>
2020-11-16 09:11:30 +0100

    Init

---
+++ UtcDateTime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UtcDateTime
+{
+  public struct UtcDateTime
+  {
+    public static TimeZoneInfo LocalTimeZoneInfo { get; set; } = TimeZoneInfo.Local;
+
+    private DateTime _utcDateTime;
+    
+    public UtcDateTime(DateTime dateTime, TimeZoneInfo timeZoneInfo = null) : this()
+    {
+      if (timeZoneInfo == null && dateTime.Kind != DateTimeKind.Utc)
+        dateTime = new DateTime(dateTime.Ticks, DateTimeKind.Utc);
+      if (timeZoneInfo != null && dateTime.Kind == DateTimeKind.Utc)
+        dateTime = new DateTime(dateTime.Ticks, DateTimeKind.Local);
+
+      _utcDateTime =  timeZoneInfo != null
+        ? TimeZoneInfo.ConvertTimeToUtc(dateTime, timeZoneInfo)
+        : dateTime;
+    }
+    public UtcDateTime(DateTime dateTime, bool useLocalTime) : this(dateTime, useLocalTime ? LocalTimeZoneInfo : null) { }
+
+    public DateTime GetDateTime(TimeZoneInfo timeZoneInfo = null)
+    {
+      if (timeZoneInfo == null || timeZoneInfo == TimeZoneInfo.Utc)
+        return _utcDateTime;
+
+      return TimeZoneInfo.ConvertTimeFromUtc(_utcDateTime, timeZoneInfo);
+    }
+    public DateTime GetDateTimeLocal()
+    {
+      return TimeZoneInfo.ConvertTimeFromUtc(_utcDateTime, LocalTimeZoneInfo);
+    }
+  }
+}
